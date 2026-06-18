dc21fa71d1dc27edce9c61e37bb900ea7c652506
niveabeh <niveasouza86@gmail.com>
2026-06-18 19:12:33 -0300

    add projeto

--- todolist/index.js
+++ todolist/index.js
@@ -4,8 +4,11 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
+
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+// Middleware para parsear o corpo das requisições
+// tratar dados vindo do formulário
 app.use(express.urlencoded({ extended: true }));
 
 
