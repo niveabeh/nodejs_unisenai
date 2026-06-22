b02815356b5d5e3564583590109c05129adc564f
niveabeh <niveasouza86@gmail.com>
2026-06-22 18:47:13 -0300

    add

---
+++ chat/public/script.js
@@ -0,0 +1,89 @@
+
+
+const socket = io();
+
+let userName = '';
+let userColor = '';
+
+const userColors = {};
+
+function generateColor(name) {
+    if (userColors[name]) return userColors[name];
+
+    let hash = 0
+
+    // Transforma a letra em um número visando a tabela ASCII, comando efetuando isso é o charCodeAt
+    for (let i = 0; i < name.length; i++) {
+
+        // pega o código em ASCII, soma com o resultado
+        hash = name.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    /// a primeira parte significa a matriz, saturação e iluminação.
+    const color = `hsl(${hash % 360}, 60% , 70%)`
+
+
+    //Inclui na array userColors
+    userColors[name] = color;
+
+    return color;
+
+
+
+}
+
+
+function enterChat() {
+
+    const nameInput = document.getElementById('userName');
+
+    const name = nameInput.value.trim();
+
+    console.log(nameInput);
+
+    if (name) {
+
+        userName = name;
+
+        userColor = generateColor(userName);
+        socket.emit('set username', userName);
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('chat').style.display = 'flex';
+    }
+}
+
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const messages = document.getElementById('messages');
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    if (input.value) {
+
+        socket.emit('chat message', input.value);
+        input.value = '';
+
+    }
+
+});
+
+socket.on('chat message', ({ name, message }) => {
+
+    const item = document.createElement('li');
+
+    const nameTag = document.createElement('span');
+
+    nameTag.textContent = name;
+    nameTag.className = 'message-name';
+    nameTag.style.backgroundColor = generateColor(name);
+
+    item.appendChild(nameTag);
+    item.append(message);
+
+    messages.appendChild(item);
+    messages.scrollTop = messages.scrollHeight;
+
+});
+
